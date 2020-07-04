d609478a1434ee9710c3058e94634ebf2d6ff34c
Aman Kumar <amankumar4real@gmail.com>
2020-07-04 18:33:35 +0530

    Added login functionality for user!

--- app/main/services/user.py
+++ app/main/services/user.py
@@ -1,6 +1,8 @@
 from ..models import db, UserModel
 import json
 import jwt
+from instance.config import SECRET_KEY
+import datetime
 
 
 def register(details):
@@ -31,12 +33,35 @@ def login(details):
         email = details["email"]
         password = details["password"]
     except KeyError:
-        return False
+        return json.dumps({"error": True,
+                           "message": "One or more fields are missing!"})
 
     if email == "" or password == "":
-        return False
+        return json.dumps({"error": True, "message": "Empty Fields"})
 
-    if type(email) is not str and type(password) is not str:
-        return False
+    if type(email) is not str or type(password) is not str:
+        return json.dumps({"error": True, "message": "Wrong data format!"})
 
-    return True
+    data = UserModel.query.filter(UserModel.email == email).first()
+
+    if data is not None:
+        if data.password == password:
+            obj = {
+                "email": data.email,
+                "type": data.type,
+                "created_at": str(datetime.datetime.utcnow()),
+                "expire_at": str(datetime.datetime.utcnow()
+                                 + datetime.timedelta(days=1))
+            }
+
+            encode_jwt = jwt.encode(obj, SECRET_KEY)
+
+            return json.dumps({"error": False, "token": encode_jwt.decode(),
+                               "message": "Logged in successfully!"})
+
+        else:
+            return json.dumps({"error": True,
+                               "message":
+                               "You have entered the wrong password!"})
+
+    return json.dumps({"error": True, "message": "Unknown error!"})
